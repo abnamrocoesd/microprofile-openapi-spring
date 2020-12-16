455b31303ed10b94ec6beb8399870ed3cc6c1ea3
dabloem <dabloem@xs4all.nl>
2020-12-16 16:48:25 +0100

    for review

--- spring-web-app/src/main/java/com/abnamro/demo/FeignClient.java
+++ spring-web-app/src/main/java/com/abnamro/demo/FeignClient.java
@@ -1,9 +1,8 @@
 package com.abnamro.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
+//@FeignClient
 public interface FeignClient {
 
     @GetMapping("/bogus")
